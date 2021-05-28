9c68a8b8c08642e7d3cfb3e07291f975d32f8d01
Leonardo Gomes Da Silva <35742346+LeonardoGomes99@users.noreply.github.com>
2021-05-28 20:07:12 -0300

    Att
    Att

---
+++ js/main.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function($) {
+
+	'use strict';
+
+        $(function() {
+  
+          // Vars
+          var modBtn  = $('#modBtn'),
+              modal   = $('#modal'),
+              close   = modal.find('.close-btn img'),
+              modContent = modal.find('.modal-content');         
+          
+        });
+
+        // on click event on all anchors with a class of scrollTo
+        $('a.scrollTo').on('click', function(){
+          
+          // data-scrollTo = section scrolling to name
+          var scrollTo = $(this).attr('data-scrollTo');
+          
+          
+          // toggle active class on and off. added 1/24/17
+          $( "a.scrollTo" ).each(function() {
+            if(scrollTo == $(this).attr('data-scrollTo')){
+              $(this).addClass('active');
+            }else{
+              $(this).removeClass('active');
+            }
+          });
+          
+          
+          // animate and scroll to the sectin 
+          $('body, html').animate({
+            
+            // the magic - scroll to section
+            "scrollTop": $('#'+scrollTo).offset().top
+          }, 1000 );
+          return false;
+          
+        })
+ 
+
+        $(".menu-icon").click(function() {
+          $(this).toggleClass("active");
+          $(".overlay-menu").toggleClass("open");
+        });
+
+});
